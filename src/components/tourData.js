5e38322dc4ec9fdba975169baa2f88a967a8a836
umit uysal <umituysal48@hotmail.com>
2020-01-07 13:23:57 +0300

    react ile yapılmış basit uygulama

---
+++ src/components/tourData.js
@@ -0,0 +1,38 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "new york",
+        img: "./img/1.jpeg",
+        name: "new york ",
+        info: "lorem lorem lorem lorem lorem"
+    },
+    {
+        id: 2,
+        city: "new york",
+        img: "./img/2.jpeg",
+        name: "new york ",
+        info: "lorem lorem lorem lorem lorem"
+    },
+    {
+        id: 3,
+        city: "new york",
+        img: "./img/5.jpeg",
+        name: "new york ",
+        info: "lorem lorem lorem lorem lorem"
+    },
+    {
+        id: 4,
+        city: "new york",
+        img: "./img/4.jpeg",
+        name: "new york ",
+        info: "lorem lorem lorem lorem lorem"
+    },
+    // {
+    //     id: 5,
+    //     city: "new york",
+    //     img: "./img/5.jpeg",
+    //     name: "new york ",
+    //     info: "lorem lorem lorem lorem lorem"
+    // }
+
+]
